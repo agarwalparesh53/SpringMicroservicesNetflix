3d36c2a650939baec23459f7edbaf925913da34e
Paresh Agarwal <agarwal.paresh53@gmail.com>
2017-06-10 00:47:01 -0500

    adding Eureka server config changes to read from the git location

--- discoveryclient/src/main/java/com/learning/microservices/DiscoveryclientApplication.java
+++ discoveryclient/src/main/java/com/learning/microservices/DiscoveryclientApplication.java
@@ -3,13 +3,13 @@ package com.learning.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaserverApplication {
+@EnableDiscoveryClient
+public class DiscoveryclientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(DiscoveryclientApplication.class, args);
 	}
 }
